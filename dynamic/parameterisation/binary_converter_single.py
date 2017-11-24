3dbba7fdd8c2a5001cb39521b3593ee99a4e7a14
mvousden <mark.vousden@soton.ac.uk>
2017-11-24 13:38:02 +0000

    Add a comment describing the previous example.

--- dynamic/parameterisation/binary_converter_single.py
+++ dynamic/parameterisation/binary_converter_single.py
@@ -9,3 +9,6 @@ def test_my_converter():
     testCase = {"input": 8,
                 "expectedResult": "1000"}
     assert integer_to_binary(testCase["input"]) == testCase["expectedResult"]
+
+    # In the previous example, this was:
+    # assert convert_integer_to_binary_string(8, 0) == "1000"
